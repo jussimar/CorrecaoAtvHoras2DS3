3052f0a6e7f418b1fc3772e723e73e47e3b53193
jussimar <jussimar@jussimarleal.com.br>
2019-10-29 12:12:34 +0900

    preenchendo campos de horas e minutos

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,29 @@
 // This is a JavaScript file
 
+
+function preencheHora(){
+    var hora = "";
+    for(var x = 0; x <= 23; x++){
+      if(x <= 9){
+        hora+="<option value="+x+">0"+x+"</option>";
+      }else{
+        hora+="<option value="+x+">"+x+"</option>";
+      }
+    }
+    $("#horaEntrada").html(hora);
+    $("#horaSaida").html(hora);
+}
+
+function preencheMinuto(){
+  var minuto = "";
+    for(var x = 0; x <= 59; x++){
+      if(x <= 9){
+        minuto+="<option value="+x+">0"+x+"</option>";
+      }else{
+        minuto+="<option value="+x+">"+x+"</option>";
+      }
+    }
+    $("#minutoEntrada").html(minuto);
+    $("#minutoSaida").html(minuto);
+}
+
